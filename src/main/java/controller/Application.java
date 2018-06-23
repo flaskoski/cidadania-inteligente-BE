9dd6ecaf6db26037308a760248df4725b9f0f44d
flaskoski <felipeflaskoski@outlook.com>
2018-06-23 17:02:04 -0300

    primitive missions API working

--- src/main/java/controller/Application.java
+++ src/main/java/controller/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 @SpringBootApplication
 public class Application {
@@ -19,13 +20,19 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
     private static void startFirebase(){
-        FileInputStream serviceAccount = new FileInputStream("path/to/serviceAccountKey.json");
+        FileInputStream serviceAccount = null;
+        try {
+            serviceAccount = new FileInputStream("firebase/cidadania-inteligente-firebase-adminsdk-r64q9-66953f20fc.json");
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://cidadania-inteligente.firebaseio.com/")
+                    .build();
 
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseUrl("https://<DATABASE_NAME>.firebaseio.com/")
-                .build();
+            FirebaseApp.initializeApp(options);
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
 
-        FirebaseApp.initializeApp(options);
+
     }
 }
--- src/main/java/controller/MissionController.java
+++ src/main/java/controller/MissionController.java
@@ -3,6 +3,10 @@ package controller;
 import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 
+import com.google.api.core.ApiFutureCallback;
+import com.google.api.core.ApiFutures;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseToken;
 import model.Mission;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,14 +19,20 @@ public class MissionController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/myMissions")
-    public Mission sendMissions(@RequestParam(value="name", defaultValue="World") String name) {
+    public ArrayList<Mission> sendMissions(@RequestParam(value="idToken", defaultValue="") String idToken) {
         final ArrayList<Mission> missions = new ArrayList<>();
-        missions.add(new Mission("Aventura no MASP",
-                "Agora você vai mostrar que sabe tudo de arte respondendo perguntas sobre obras de arte presentes num dos pontos mais famosos de São Paulo, o MASP!"));
-        missions.add(new Mission("Em busca do tesouro...", ""));
-
-        return new Mission("Aventura no MASP",
-                "Agora você vai mostrar que sabe tudo de arte respondendo perguntas sobre obras de arte presentes num dos pontos mais famosos de São Paulo, o MASP!");
+        ApiFutures.addCallback(FirebaseAuth.getInstance().verifyIdTokenAsync(idToken),
+        new ApiFutureCallback<FirebaseToken>() {
+            @Override
+            public void onFailure(Throwable t) {}
+            @Override
+            public void onSuccess(FirebaseToken decodedToken) {
+                missions.add(new Mission("Aventura no MASP",
+                        "Agora você vai mostrar que sabe tudo de arte respondendo perguntas sobre obras de arte presentes num dos pontos mais famosos de São Paulo, o MASP!"));
+                missions.add(new Mission("Em busca do tesouro...", ""));
+            }
+        });
+        return missions;
     }
 
 }
