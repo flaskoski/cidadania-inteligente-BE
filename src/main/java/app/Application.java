48a29aa1dc840e40a7af1b0870fec8997ea10b38
flaskoski <felipeflaskoski@outlook.com>
2018-07-17 20:47:56 -0300

    mongodb fully working for retrieving missions (without secure connection)

---
+++ src/main/java/app/Application.java
@@ -0,0 +1,42 @@
+package app;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+//import MissionRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.FileInputStream;
+
+@SpringBootApplication
+public class Application {
+
+
+    public static void main(String[] args) {
+
+        startFirebase();
+
+     //   addItemToDB();
+
+
+        SpringApplication.run(Application.class, args);
+    }
+
+    private static void startFirebase(){
+        FileInputStream serviceAccount = null;
+        try {
+            serviceAccount = new FileInputStream("resources/cidadania-inteligente-firebase-adminsdk-r64q9-66953f20fc.json");
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://cidadania-inteligente.firebaseio.com/")
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
