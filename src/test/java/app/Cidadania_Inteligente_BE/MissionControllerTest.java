633034cab2fa4869d8052d9780b3cfd85733c5a1
flaskoski <felipeflaskoski@outlook.com>
2018-09-05 22:49:00 -0300

    tests moved

--- src/test/java/app/Cidadania_Inteligente_BE/MissionControllerTest.java
+++ src/test/java/app/Cidadania_Inteligente_BE/MissionControllerTest.java
@@ -1,10 +1,10 @@
-package app.controller;
+package app.Cidadania_Inteligente_BE;
 
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
 
 class MissionControllerTest {
 
-    @org.junit.jupiter.api.Test
+    //@org.junit.jupiter.api.Test
     void sendMissions() {
         //TODO initialize with a user from FB and test API json return.
     }
