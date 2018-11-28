ae507eb9ee4e196e43e8f9a7887b325bfd0249b3
flaskoski <felipeflaskoski@outlook.com>
2018-11-28 00:21:40 -0200

    Tasks being sent in the correct order Println -> Logger on TaskController

--- src/main/java/app/controller/TaskController.java
+++ src/main/java/app/controller/TaskController.java
@@ -2,14 +2,18 @@ package app.controller;
 
 import java.util.*;
 
+import app.fileManagement.FileController;
 import app.firebase.FirebaseValidator;
 import app.model.AbstractTask;
 import app.model.QuestionTask;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TaskController {
+    private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
 
     @Autowired
     private TasksRepository tasksRepository;
@@ -23,30 +27,33 @@ public class TaskController {
         final ArrayList<AbstractTask> tasks= new ArrayList<>();
         String idToken = headers.get("Authorization".toLowerCase());
         if(idToken != null)
-            System.out.print("token caught!");
+            logger.info("token caught!");
         else return null;
 //        if (StringUtils.isBlank(idToken)) {
 //            throw new IllegalArgumentException("FirebaseTokenBlank");
 //        }
 
+        //Sort tasks by key (task0, task1, ...)
+        Map<String,String> sortedParams = new TreeMap<>(params);
+
         String playerUid = FirebaseValidator.validateUser(idToken);
         if(playerUid == null)
             return null;
 
-        System.out.println("Retrieving tasks progress...");
+        logger.info("Retrieving tasks progress...");
         List<String> taskIDs = new ArrayList<>();
-        Iterator<Map.Entry<String, String>> it = params.entrySet().iterator();
+        Iterator<Map.Entry<String, String>> it = sortedParams.entrySet().iterator();
         while(it.hasNext()){
             Map.Entry<String,String> entry = it.next();
             if(entry.getKey().toLowerCase().contains("taskid"))
                 taskIDs.add(entry.getValue());
         }
-        System.out.println("TaskIDs: "+taskIDs.toString());
+        logger.info("TaskIDs: "+taskIDs.toString());
         for (String taskId : taskIDs){
             tasksRepository.findById(taskId).ifPresent(tasks::add);
         }
 
-        System.out.println(tasks.size());
+        logger.info(String.valueOf(tasks.size()));
         return tasks;
     }
 
